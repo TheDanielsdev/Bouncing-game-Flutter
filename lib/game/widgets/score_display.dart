5be6f1cfcadc9604353cff1c1dba4014fc9eeafd
TheDanielsdev <thedanielsdev@gmail.com>
2023-02-06 09:24:41 +0100

    first commit

---
+++ lib/game/widgets/score_display.dart
@@ -0,0 +1,30 @@
+// Copyright 2022 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+import '../doodle_dash.dart';
+
+class ScoreDisplay extends StatelessWidget {
+  ScoreDisplay({
+    super.key,
+    required this.game,
+    this.isLight = false,
+  });
+
+  final Game game;
+  final bool isLight;
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: (game as DoodleDash).gameManager.score,
+      builder: (context, value, child) {
+        return Text(value > 100 ? 'New High Score: $value' : 'Score: $value',
+            style: Theme.of(context).textTheme.displaySmall!);
+      },
+    );
+  }
+}
